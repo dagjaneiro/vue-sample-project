68077d11d5f6f37c0ce02eed9bbed5ea86c086dc
dagjaneiro <dag.janeiro@gmail.com>
2018-08-23 16:57:43 +0100

    testing: add cypress mock test

--- test/e2e/specs/feed.spec.js
+++ test/e2e/specs/feed.spec.js
@@ -10,13 +10,38 @@ describe('Post Feed', () => {
 
     cy.wait('@posts')
 
-    cy.get('li > ul').should('have.length', 101)
+    cy.get('li > ul').should('have.length', 100)
   })
 
   describe('when we click on a blog post', () => {
     it('navigates to the blog post', () => {
       cy.visit('')
       cy.contains('qui est esse').click()
+
+      cy.url().should('contain', '/posts/2')
+    })
+  })
+})
+
+describe('Integration - Post Feed', () => {
+  beforeEach(() => {
+    // intercept ajax /posts request
+    cy.server()
+    cy.route('GET', '/posts', 'fixture:posts.json').as('posts')
+  })
+
+  it('renders the first 100 blog posts', () => {
+    cy.visit('')
+
+    cy.wait('@posts')
+
+    cy.get('li > ul').should('have.length', 3)
+  })
+
+  describe('when we click on a blog post', () => {
+    it('navigates to the blog post', () => {
+      cy.visit('')
+      cy.contains('stubbed blog post 2').click()
 
       cy.url().should('contain', '/posts/2')
     })
